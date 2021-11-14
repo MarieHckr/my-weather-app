5ad7ccdd2613131cee4b84c8a3183e5e8c914838
MarieHckr <84939047+MarieHckr@users.noreply.github.com>
2021-11-14 19:14:58 +0100

    changed the colors

--- src/index2.js
+++ src/index2.js
@@ -47,7 +47,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thur", "Fri", "Sat", "Sun"];
+  let days = ["Thur", "Fri", "Sat", "Sun", "Mon"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
